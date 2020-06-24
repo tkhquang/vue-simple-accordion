5f9b92ec0de0aad6e4d5062e2b46cf1273dbeba4
Quang Trinh <khacquang.trinh@gmail.com>
2020-06-25 06:02:26 +0700

    Fix docs index title

--- docs-src/vue.config.js
+++ docs-src/vue.config.js
@@ -1,5 +1,11 @@
 const path = require("path");
 
 module.exports = {
-  outputDir: path.resolve(__dirname, "../docs")
+  outputDir: path.resolve(__dirname, "../docs"),
+  chainWebpack: (config) => {
+    config.plugin("html").tap((args) => {
+      args[0].title = "Vue Simple Accordion";
+      return args;
+    });
+  }
 };
