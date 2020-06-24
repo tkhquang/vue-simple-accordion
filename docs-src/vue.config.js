b4f94180354b24c601ba363112baba2f15d76f49
Quang Trinh <khacquang.trinh@gmail.com>
2020-06-25 06:27:58 +0700

    Fix docs path

--- docs-src/vue.config.js
+++ docs-src/vue.config.js
@@ -2,6 +2,8 @@ const path = require("path");
 
 module.exports = {
   outputDir: path.resolve(__dirname, "../docs"),
+  publicPath:
+    process.env.NODE_ENV === "production" ? "/vue-simple-accordion/" : "/",
   chainWebpack: (config) => {
     config.plugin("html").tap((args) => {
       args[0].title = "Vue Simple Accordion";
